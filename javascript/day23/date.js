06f34a6598dd72dd90dff30baf0aef136937839a
HojinAn <asdv94@naver.com>
2021-12-01 21:54:01 +0900

    Day23 JavaScript date

---
+++ javascript/day23/date.js
@@ -0,0 +1,25 @@
+// 날짜
+let d = new Date();
+d // Wed Dec 01 2021 10:35:01 GMT+0900 (한국 표준시)
+d.getMonth(); // 11
+d.getDate(); // 1
+// 월은 0부터 시작합니다.
+// 일은 1부터 시작합니다.
+d.getDay(); // 3
+// 일요일부터 0입니다.
+d.getHours();
+10
+d.getMinutes();
+35
+d.getSeconds();
+1
+d.getMinutes();
+35
+let dd = new Date();
+undefined
+dd.getMinutes();
+39
+// 실시간 아니고 new Date() 했을 때에 시간
+
+d.getFullYear(); // 2021
+d.getYear(); // 121 이거는 2000년 이후로는 안쓴다.
