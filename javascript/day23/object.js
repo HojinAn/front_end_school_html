98f7aa6a9012cf3d529558bab2062cae5cb38f19
HojinAn <asdv94@naver.com>
2021-12-01 21:54:54 +0900

    Day23 JavaScript object

---
+++ javascript/day23/object.js
@@ -0,0 +1,11 @@
+let person = {
+    // key: value;
+    name: '이호준',
+    age: 10,
+    height: 30,
+    weight: 40,
+    이력: {
+        '첫번째직장': '하나', '두번째직장': '둘'
+    }
+};
+console.log(person.name);
