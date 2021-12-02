d8672b883897d6f58244509d36287afd4cbfcc5b
HojinAn <asdv94@naver.com>
2021-12-02 22:08:07 +0900

    Day24 JavaScript practice

---
+++ javascript/day24/test2.js
@@ -0,0 +1,17 @@
+let m = new Map();
+m.set('하나', 'one');
+m.set('둘', 'two');
+m.set('셋', 'three');
+console.log(m);
+console.log(m.keys());
+// let arr = new Array(1, 2, 3, 4);
+// let arr2 = arr;
+// let arr3 = [...arr];
+// console.log(arr2 === arr); // true
+// console.log(arr2 === arr3); // false
+
+// arr2 = [3, 4, 5];
+// console.log('arr=', arr);
+// console.log('arr2=', arr2);
+// console.log('arr3=', arr3);
+console.log(m.entries());
