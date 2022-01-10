d59f0d8d4c804d7451c191b96837dc3d3209d08e
HojinAn <asdv94@naver.com>
2022-01-11 00:38:25 +0900

    Day40 routing

--- node/day40/js_test/first-project/routes/index.js
+++ node/day40/js_test/first-project/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router get으로 "/"" 요청 받고 콜백 함수로 req, res, next 처리
+// routing의 시작은 router
+
+router.post("/main", (req, res) => {
+  // res.json({
+  //   message: "success!!!",
+  // });
+  // console.log(req.body);
+  // const data = req.body.data;
+  // res.send("문자열이 응답됩니다!");
+  res.render("index");
 });
 
 module.exports = router;
