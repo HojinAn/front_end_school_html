f82c0ab8a9cc40a9da9dafcdf01995a7b1939b43
HojinAn <asdv94@naver.com>
2022-01-11 22:15:33 +0900

    Day40 express-http method

--- node/day40/js_test/first-project/routes/index.js
+++ node/day40/js_test/first-project/routes/index.js
@@ -1,21 +1,55 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-// router get으로 "/"" 요청 받고 콜백 함수로 req, res, next 처리
-// routing의 시작은 router
+let arr = [];
 
-router.post("/main", (req, res) => {
-  // res.json({
-  //   message: "success!!!",
-  // });
-  // console.log(req.body);
+// GET method
+
+router.get("/read", (req, res) => {
+  res.status(200).json({
+    message: "read success",
+  });
+});
+
+// POST method
+
+router.post("/create", (req, res) => {
   // const data = req.body.data;
-  // res.send("문자열이 응답됩니다!");
-  res.render("index");
+  const { data } = req.body; // 위랑 같은 코드 비구조화 할당
+  arr.push(data);
+  res.status(200).json({
+    message: "create success",
+    result: arr,
+  });
+  // console.log(req.body);
+  // // postman에서 send해주면 Body에 raw JSON으로 작성한 object가 console로 찍힌다.
+});
+
+// PUT method
+
+// update/0
+// update/2 // 이런식으로
+
+router.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  // console.log(id);
+  const { data } = req.body;
+  arr[id] = data;
+  res.status(200).json({
+    message: "update success",
+    result: arr,
+  });
+});
+
+// DELETE method
+
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  arr.splice(id, 1);
+  res.status(200).json({
+    message: "delete success",
+    result: arr,
+  });
 });
 
 module.exports = router;
