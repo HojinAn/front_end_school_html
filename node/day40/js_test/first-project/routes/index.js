63410bfbe0e771cdc79ff75df4c2e6331701ca22
HojinAn <asdv94@naver.com>
2022-01-11 23:11:24 +0900

    Day40 middleware

--- node/day40/js_test/first-project/routes/index.js
+++ node/day40/js_test/first-project/routes/index.js
@@ -1,54 +1,22 @@
 var express = require("express");
 var router = express.Router();
+const loginCheck = require("../module/loginCheck");
 
-let arr = [];
+// const loginCheck = (req, res, next) => {
+//   const userLogin = true;
+//   if (userLogin) {
+//     next();
+//   } else {
+//     res.status(400).json({
+//       message: "login fail!",
+//     });
+//   }
+// };
+// //  index.js에는 server logic만 만들어줘야 하는데 이렇게 middleware 함수가 들어와있으면 번잡해진다. 따라서 module 폴더로 따로 빼준다.
 
-// GET method
-
-router.get("/read", (req, res) => {
+router.get("/", loginCheck, (req, res) => {
   res.status(200).json({
-    message: "read success",
-  });
-});
-
-// POST method
-
-router.post("/create", (req, res) => {
-  // const data = req.body.data;
-  const { data } = req.body; // 위랑 같은 코드 비구조화 할당
-  arr.push(data);
-  res.status(200).json({
-    message: "create success",
-    result: arr,
-  });
-  // console.log(req.body);
-  // // postman에서 send해주면 Body에 raw JSON으로 작성한 object가 console로 찍힌다.
-});
-
-// PUT method
-
-// update/0
-// update/2 // 이런식으로
-
-router.put("/update/:id", (req, res) => {
-  const { id } = req.params;
-  // console.log(id);
-  const { data } = req.body;
-  arr[id] = data;
-  res.status(200).json({
-    message: "update success",
-    result: arr,
-  });
-});
-
-// DELETE method
-
-router.delete("/delete/:id", (req, res) => {
-  const { id } = req.params;
-  arr.splice(id, 1);
-  res.status(200).json({
-    message: "delete success",
-    result: arr,
+    message: "login success!!",
   });
 });
 
